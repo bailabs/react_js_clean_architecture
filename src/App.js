6afd86e7aad526aecc0b91d36f9abed5ab749eb2
Chris Ian Fiel <ccfiel@gmail.com>
2020-03-08 18:00:39 +0800

    install prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import React from "react"
+import { Provider } from "react-redux"
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
 
-import ItemList from "./app/pages/item/ItemList";
-import items from "./app/pages/item/Item.reducers";
+import ItemList from "./app/pages/item/ItemList"
+import items from "./app/pages/item/Item.reducers"
 
 // Setup Redux store with Thunks
-const reducers = combineReducers({ items });
-const store = createStore(reducers, applyMiddleware(thunk));
+const reducers = combineReducers({ items })
+const store = createStore(reducers, applyMiddleware(thunk))
 
 const App = () => (
-  <Provider store={store}>
-    <ItemList />
-  </Provider>
-);
+    <Provider store={store}>
+        <ItemList />
+    </Provider>
+)
 
-export default App;
+export default App
