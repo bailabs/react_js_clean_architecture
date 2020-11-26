050e4ebf29d566548f51211931fab6c037f6bb51
Chris Ian Fiel <ccfiel@gmail.com>
2020-11-26 19:44:49 +0800

    fix: package to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
-module.exports =  {
-    parser:  '@typescript-eslint/parser',
-    extends:  [
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-        'plugin:react/recommended',
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+        "plugin:react/recommended",
     ],
-    parserOptions:  {
-        ecmaVersion:  2018,
-        sourceType:  'module',
-        ecmaFeatures:  {
-            jsx:  true,
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
         },
     },
-    rules:  {
+    rules: {
         "@typescript-eslint/explicit-function-return-type": "off",
     },
-    settings:  {
-        react:  {
-            version:  'detect',
+    settings: {
+        react: {
+            version: "detect",
         },
     },
-};
+}
