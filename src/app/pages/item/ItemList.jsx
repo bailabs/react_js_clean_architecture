9c2752421ae74e0d84cff6eab3bd643f5106681f
tabacalera <calerataba@gmail.com>
2020-11-30 15:41:52 +0800

    added prop validation and turned off explicit return types

--- src/app/pages/item/ItemList.jsx
+++ src/app/pages/item/ItemList.jsx
@@ -1,17 +1,21 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { connect } from "react-redux"
 import { refreshList } from "./Item.actions"
 
-const ItemList = ({ items, refreshList }) => (
-    <div>
-        <button onClick={refreshList}>Refresh</button>
-        <ul>
-            {items.map((item) => (
-                <li key={item.id}>{item.name}</li>
-            ))}
-        </ul>
-    </div>
-)
+const ItemList = ({ items, refreshList }) => {
+    console.log(items)
+    return (
+        <div>
+            <button onClick={refreshList}>Refresh</button>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => ({
     items: state.items.items,
@@ -20,5 +24,13 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
     refreshList: () => dispatch(refreshList),
 })
-
+ItemList.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            name: PropTypes.string,
+        }),
+    ),
+    refreshList: PropTypes.func,
+}
 export default connect(mapStateToProps, mapDispatchToProps)(ItemList)
